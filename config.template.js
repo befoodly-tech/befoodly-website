029cde9b94e8792c94c084bb6e676425de40b729
Deepak Jangid <127429178+Deepakbefoodly@users.noreply.github.com>
2023-04-12 22:41:33 +0530

    fixes all font issues and make basic setup (#4)

---
+++ config.template.js
@@ -0,0 +1,3 @@
+window.config = {
+    BASE_URL: '<BASE_URL>'
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
+import { AppConfig } from "./types/AppConfig";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@emotion/react";
+
+declare global {
+  interface Window {
+    config: AppConfig;
+  }
+}
+
+const config = window.config ?? {};
+
+const initOptions = {
+  url: config.BASE_URL
+}
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Cabin, Roboto, serif, Times New Roman'
+  },
+  status: {
+    danger: 'red'
+  }
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Navbar />
       <Home />
-    </>
+    </ThemeProvider>
   );
 }
 
