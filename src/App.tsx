5a6a2e03bdc2928210e7027f185a7dd4dfc1167b
Deepak Jangid <127429178+Deepakbefoodly@users.noreply.github.com>
2023-04-16 00:21:42 +0530

    fixes all project issues (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 import { AppConfig } from './types/AppConfig';
-import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
+import { theme } from './ui/theme';
 
 declare global {
   interface Window {
@@ -16,15 +16,6 @@ const config = window.config ?? {};
 const initOptions = {
   url: config.BASE_URL
 };
-
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Cabin, Roboto, serif, Times New Roman'
-  },
-  status: {
-    danger: 'red'
-  }
-});
 
 function App() {
   return (
