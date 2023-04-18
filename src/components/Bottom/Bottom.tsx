085ab385fb57f4d71c0e4a0c8296a986666b320f
piyushchouhan <piyush1.befoodly@gmail.com>
2023-04-18 22:49:55 +0530

    Responsive event section

--- src/components/Bottom/Bottom.tsx
+++ src/components/Bottom/Bottom.tsx
@@ -1,42 +1,34 @@
 import React from 'react';
-import { Box, Container } from '@mui/system';
+import { Box } from '@mui/system';
 import { Typography } from '@mui/material';
+import styles from './Bottom.module.css';
 
 const Bottom = () => {
   return (
-    <Box sx={{ background: '#F3E32B', paddingTop: '5px' }} marginTop={5} height={'800'}>
-      <Container>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between'
-          }}
-        >
-          <Box paddingLeft={15} paddingY={20}>
-            <Typography variant="h4">01/07/2023</Typography>
-          </Box>
-          <Box paddingX={5} paddingY={10}>
-            <Typography textAlign="center" variant="h2">
-              Event
+    <Box className={styles.intro}>
+      <Typography variant="h3">Events</Typography>
+      {/* <Box className={styles.seprator}>
+        <hr></hr>
+      </Box> */}
+      <Box className={styles.mid}>
+        <Box className={styles.midblock}>
+          <Typography variant="h5">01/07/2023</Typography>
+        </Box>
+        <Box className={styles.midblock}>
+          <Box className={styles.launch}>
+            <Typography variant="h5">We Are Launching In Bangalore!</Typography>
+            <Typography variant="subtitle1" className={styles.subtext}>
+              Our mobile application will launch by first week of the July, 2023 and initially we
+              will start providing service on Suncity lane, Bellandur....
             </Typography>
-            <hr />
-            <Box>
-              <Typography variant="h5" paddingY={2}>
-                We Are Launching In Bangalore!
-              </Typography>
-              <Typography sx={{ opacity: '70%' }}>
-                Our mobile application will launch by first week of the July, 2023 and initially we
-                will start providing service on Suncity lane, Bellandur....
-              </Typography>
-            </Box>
-          </Box>
-          <Box paddingRight={15} paddingY={20}>
-            <Typography variant="h5">11am&#8209;10pm</Typography>
-            <Typography>Bellandur</Typography>
           </Box>
         </Box>
-        <hr />
-      </Container>
+        <Box className={styles.midblock}>
+          <Typography variant="h5">11am- 10pm</Typography>
+          <Typography>Bellandur</Typography>
+        </Box>
+      </Box>
+      <hr></hr>
     </Box>
   );
 };
