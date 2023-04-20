54b247ba5194fef6307a8f2ec6aa561379416fdd
piyushchouhan <piyush1.befoodly@gmail.com>
2023-04-20 10:57:57 +0530

    Added responsive scroll and bottom site change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,9 +4,27 @@ import styles from './Navbar.module.css';
 
 const options = ['Home', 'About', 'Blog'];
 
+//Handle Scroll
+window.addEventListener('scroll', function (event) {
+  event.preventDefault();
+  const scr = this.scrollY;
+  const appBar = this.document.getElementById('appbar');
+  if (appBar) {
+    if (scr > 5) {
+      {
+        appBar.style.backgroundColor = 'black';
+        appBar.style.opacity = '70%';
+      }
+    } else {
+      appBar.style.backgroundColor = 'transparent';
+      appBar.style.opacity = '';
+    }
+  }
+});
+
 const Navbar = () => {
   return (
-    <AppBar sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+    <AppBar id="appbar" sx={{ backgroundColor: 'transparent', boxShadow: 'none', opacity: '100%' }}>
       <Container>
         <Toolbar className={styles.toolbar}>
           <Box>
