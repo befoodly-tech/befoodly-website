623141efa9e19e57eebced464bc0c25fddbd216f
piyushchouhan <piyush1.befoodly@gmail.com>
2023-07-02 12:08:49 +0530

    Navbar update

--- src/components/NavbarApp/NavbarApp.tsx
+++ src/components/NavbarApp/NavbarApp.tsx
@@ -12,59 +12,56 @@ import BefoodlyLogo from '../../assets/svgs/LogoBlack.svg';
 import Search from '../../ui/Icon/Search/Search';
 import HomeIcon from '../../ui/Icon/Home/Home';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  color: theme.palette.text.secondary,
-  height: '3.375rem',
-  width: '44rem',
-  lineHeight: '60px',
-  paddingLeft: '16px',
-  paddingRight: '16px'
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   ...theme.typography.body2,
+//   color: theme.palette.text.secondary,
+//   height: '3.375rem',
+//   width: '44rem',
+//   lineHeight: '60px',
+//   paddingLeft: '16px',
+//   paddingRight: '16px'
+// }));
 const NavbarApp = () => {
   return (
     <Box className={styles.head}>
       <Container className={styles.main}>
         <Box component={'img'} src={BefoodlyLogo} alt="Befoodly Logo"></Box>
-        <Item>
-          <Box className={styles.find}>
-            <Button
-              color="primary"
-              variant="text"
-              aria-haspopup="true"
-              className={styles.place}
-              startIcon={<HomeIcon />}
-              endIcon={<KeyboardArrowDownIcon color="secondary" />}
-            >
-              <Box>
-                <Typography color={'black'}>Home</Typography>
-                <Typography color={'black'}>Bellandure</Typography>
-              </Box>
-            </Button>
-            <Box className={styles.inputField}>
-              <TextField
-                variant="standard"
-                placeholder="Search for home cooked food"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Search />
-                    </InputAdornment>
-                  ),
-                  disableUnderline: true
-                }}
-                fullWidth
-              ></TextField>
+        <Paper className={styles.find}>
+          <Button
+            color="primary"
+            variant="text"
+            className={styles.placeBtn}
+            startIcon={<HomeIcon className={styles.placeLogo} />}
+            endIcon={<KeyboardArrowDownIcon color="secondary" />}
+          >
+            <Box className={styles.place}>
+              <Typography className={styles.placeName}>Home</Typography>
+              <Typography className={styles.placeArea}>Bellandure, Bangalore</Typography>
             </Box>
+          </Button>
+          <Box className={styles.inputField}>
+            <TextField
+              variant="standard"
+              placeholder="Search for home cooked food"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Search />
+                  </InputAdornment>
+                ),
+                disableUnderline: true
+              }}
+              fullWidth
+            ></TextField>
           </Box>
-        </Item>
-        <Box>
-          <Button variant="contained" color="secondary">
+        </Paper>
+        <Box className={styles.checkin}>
+          <Button className={styles.checkinBtn} color="primary">
             Login
           </Button>
-          <Button variant="contained" color="secondary">
+          <Button className={styles.checkinBtn} color="primary">
             SignUp
           </Button>
         </Box>
